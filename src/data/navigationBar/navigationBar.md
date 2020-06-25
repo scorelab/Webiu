669a64b6d6d5a412a071b42d9c262da48cb96c8a
Devon Wijesinghe <wdevon99@gmail.com>
2020-06-25 22:21:36 +0530

    Adds footer component and data config for footer and nav bar

---
+++ src/data/navigationBar/navigationBar.md
@@ -0,0 +1,17 @@
+---
+template: 'navigationBar'
+
+logo: /logos/main-logo.jpeg
+pages:
+  - name: Home
+    path: /
+  - name: Projects
+    path: /projects
+  - name: Publications
+    path: /publications
+  - name: Blog
+    path: /blog
+  - name: GSOC 2020
+    path: /gsoc
+    isSpecial: true
+---
--- src/layouts/MainLayout/index.js
+++ src/layouts/MainLayout/index.js
@@ -1,19 +1,29 @@
 import React from "react"
 import PropTypes from "prop-types"
-import NavBar from '../../components/NavBar'
 import pages from '../../config/pages'
+import NavBar from '../../components/NavBar'
+import Footer from '../../components/Footer'
+import { useFooterData } from '../../queries/footer'
+import { useNavigationBarData } from '../../queries/navigationBar'
 import './style.sass'
 
 const MainLayout = ({ children }) => {
+  const footerData = useFooterData()
+  const navigationBarData = useNavigationBarData()
+    
   return (
       <div className="main-layout-component">
         <NavBar 
-          logo={'/logos/main-logo.jpeg'}
-          links={pages}
+          logo={navigationBarData.logo}
+          links={navigationBarData.pages}
         />
         <div className="content-area">
           {children}
         </div>
+        <Footer 
+          mainText={footerData.mainText}
+          socialLinks={footerData.socialLinks}
+        />
       </div>
   )
 }
