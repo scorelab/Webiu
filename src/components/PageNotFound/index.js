f5eff13bbd43ed081899c26f6cb20269169a207b
Devon Wijesinghe <wdevon99@gmail.com>
2020-06-26 12:16:30 +0530

    Adds page not not find

---
+++ src/components/PageNotFound/index.js
@@ -0,0 +1,26 @@
+import React from "react"
+import PropTypes from "prop-types"
+import "./style.sass"
+import { Link } from "gatsby"
+
+const PageNotFound = ({ image, mainText, subText }) => {
+  return (
+    <div className="page-not-found-component">
+      <div className="inner-container">
+        <img src={image} alt='404'/>
+          <h1>{mainText}</h1>
+          <p>
+            {subText} | Return to <Link to='/'>Home</Link> page
+          </p>
+      </div>
+    </div>
+  )
+}
+
+PageNotFound.propTypes = {
+  image: PropTypes.string,
+  mainText: PropTypes.string,
+  subText: PropTypes.string,
+}
+
+export default PageNotFound
