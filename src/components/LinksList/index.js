c25f04a05309b76f70c1f4ad03cd8e9d2aecdbab
Yash Vardhan <yashvardhan513@gmail.com>
2021-02-10 10:47:19 +0530

    update links component to include an icon

--- src/components/LinksList/index.js
+++ src/components/LinksList/index.js
@@ -1,23 +1,28 @@
 import React from "react"
 import PropTypes from "prop-types"
-import "./style.sass"
+import "./style.sass";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
-export const LinksList = ({links}) => {
+export const LinksList = ({links, isIcon}) => {
+
   return (
     <div className="links-list-component">
       {links ? (
         <ul>
           {links.map((link, i) => (
             <li key={`${link.url}-${i}`}>
-              <span>{link.name}</span>: <a href={link.url}>{link.url}</a>
+              <span>{link.name}</span>:
+              <a href={link.url}>{link.url} {isIcon ? <FontAwesomeIcon icon={faExternalLinkAlt} /> : null}</a>
             </li>
           ))}
         </ul>
       ) : <p>No links found!</p>}
-    </div>
+     </div>
   )
 }
 
 LinksList.propTypes = {
   links: PropTypes.array,
+  isIcon: PropTypes.bool,
 }
