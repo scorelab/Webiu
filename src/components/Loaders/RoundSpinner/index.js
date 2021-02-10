7a5b9977da73481816478525a91614790e151a41
sachin <sachinsom507@gmail.com>
2021-02-10 13:37:12 -0800

    Added A Loader component It Contains 1. BouncingBall Loader 2. SquareSpinner Loader 3. RoundSpinner Loader 4. VerticalLines Loader

---
+++ src/components/Loaders/RoundSpinner/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./style.sass"
+
+export const RoundSpinnerLoader = () => {
+    return (
+        <div className="roundSpinner-container">
+            <div className="roundSpinner-wrapper">
+                <div></div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Loaders/VerticalLines/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./style.sass"
+
+export const VerticalLines = () => {
+    return (
+        <div className="verticalLines-container">
+            <div className="verticalLines-wrapper">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
