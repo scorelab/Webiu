f4ba3bcf5931ebec10bf059eb26c6e57027d3bec
Yash Vardhan <yashvardhan513@gmail.com>
2021-02-21 16:14:06 +0530

    built the faq component from scratch

--- src/components/FaqComponent/index.js
+++ src/components/FaqComponent/index.js
@@ -1,23 +1,34 @@
-import React from "react"
+import React from "react";
+import ReactDOM from "react-dom";
 import PropTypes from "prop-types"
-import Faq from 'react-faq-component';
+import Accordion from "./Accordion";
 import {Container, Row, Col} from 'react-bootstrap'
-import "./style.sass";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import { faQuestion } from '@fortawesome/free-solid-svg-icons';
 
-export const FaqComponent = ({data}) => {
+import "./style.sass";
 
+export const FaqComponent = ({data, header}) => {
   return (
     <div>
+    { header ?
       <div className="header-component">
-        <h3><FontAwesomeIcon className="icon" icon={faQuestionCircle} />{data.title}</h3>
+       <h3>{header} <FontAwesomeIcon className="icon" icon={faQuestion} /></h3>
       </div>
-      <Container>
-      <div>
-        <Faq data={data}/>
-      </div>
-      </Container>
+     : null }
+    <Container className="faq-container">
+      {data.length >=1 ? data.map(faq => (
+        <Accordion
+          title={faq.title}
+          content={faq.content}
+        />
+      )) : null}
+    </Container>
     </div>
-  )
+  );
+}
+
+Faq.propTypes = {
+  data: PropTypes.array,
+  heading: PropTypes.string
 }
