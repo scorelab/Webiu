e7f385f2a6f4eca77cf4c5d54a31203c75c605a8
Nand Kishor <nandkk07@gmail.com>
2021-03-31 11:23:18 +0530

    testimonial component added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
+import { Testimonials } from "../components/Testimonials"
 
 const ExamplePage = () => {
   return (
     <div>
+      <Testimonials />
     </div>
   )
 }
