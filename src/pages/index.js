ca6a245d99cf84fd9fa6f2c982b701437ad9f841
Nand Kishor <nandkk07@gmail.com>
2021-04-02 15:28:56 +0530

    localhost page issue resolved

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import React from "react"
-import { Testimonials } from "../components/Testimonials"
 
 const ExamplePage = () => {
   return (
-    <div>
-      <Testimonials />
+    <div
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <h3>Add your components to the Pages/index.js</h3>
     </div>
   )
 }
