0d7188a9ea861a9de8a146ed7a5fb4c1db8f4bb4
SaurabhSonde <saurabhsonde111@gmail.com>
2021-06-07 14:36:17 +0530

    removed dangerouslySetInnerHTML to solve the issue

--- src/components/GsocIdeaList/index.js
+++ src/components/GsocIdeaList/index.js
@@ -1,37 +1,42 @@
 import React from "react"
 import PropTypes from "prop-types"
 import "./style.sass"
-import {Container, Row, Col} from 'react-bootstrap'
-import { Collapse } from 'antd';
+import { Container, Row, Col } from "react-bootstrap"
+import { Collapse } from "antd"
 
-const { Panel } = Collapse;
+const { Panel } = Collapse
 
-export const GsocIdeaList = ({ heading, description, listItems, defaultActiveKeys = []}) => {
+export const GsocIdeaList = ({
+  heading,
+  description,
+  listItems,
+  defaultActiveKeys = [],
+}) => {
   return (
     <div className="gsoc-idea-list-component">
       <Container>
         <Row>
-          <Col> 
+          <Col>
             {!heading || <h1>{heading}</h1>}
             {!description || <h2>{description}</h2>}
 
             <Collapse defaultActiveKey={defaultActiveKeys}>
-              {!listItems || listItems.map((item, i) => (
-                <Panel header={item.title} key={`${i}`}>
-                  <h4>Description</h4>
-                  <p dangerouslySetInnerHTML={{ __html: item.description}}></p>
-                  <h4>Expected Results</h4>
-                  <p dangerouslySetInnerHTML={{ __html: item.expectedresults}}></p>
-                  <h4>Required Knowledge</h4>
-                  <p>{item.requiredknowledge}</p>
-                  <h4>Possible Mentors</h4>
-                  <p>{item.possiblementors}</p>
-                  <h4>Github Url</h4>
-                  <a href={item.githuburl}>{item.githuburl}</a>
-                </Panel>
-              ))}
+              {!listItems ||
+                listItems.map((item, i) => (
+                  <Panel header={item.title} key={`${i}`}>
+                    <h4>Description</h4>
+                    <p>{item.description}</p>
+                    <h4>Expected Results</h4>
+                    <p>{item.expectedresults}</p>
+                    <h4>Required Knowledge</h4>
+                    <p>{item.requiredknowledge}</p>
+                    <h4>Possible Mentors</h4>
+                    <p>{item.possiblementors}</p>
+                    <h4>Github Url</h4>
+                    <a href={item.githuburl}>{item.githuburl}</a>
+                  </Panel>
+                ))}
             </Collapse>
-
           </Col>
         </Row>
       </Container>
@@ -40,7 +45,7 @@ export const GsocIdeaList = ({ heading, description, listItems, defaultActiveKey
 }
 
 GsocIdeaList.propTypes = {
-  heading: PropTypes.string, 
+  heading: PropTypes.string,
   description: PropTypes.string,
   listItems: PropTypes.array,
   defaultActiveKeys: PropTypes.array,
