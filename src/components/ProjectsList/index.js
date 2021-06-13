e12f880d7e56a5f7c1a31f11f343a5838b1eb1c8
Yash Vardhan <yashvardhan513@gmail.com>
2021-06-13 12:58:51 +0530

    add the search function to project list

--- src/components/ProjectsList/index.js
+++ src/components/ProjectsList/index.js
@@ -1,27 +1,46 @@
-import React from "react"
+import React, {useState} from "react"
 import PropTypes from "prop-types"
 import "./style.sass"
 import {Container, Row, Col} from 'react-bootstrap'
 import ProjectListItem from './ProjectListItem'
 import {Link} from "gatsby"
+import {SearchBar} from "../SearchBar";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faCode } from "@fortawesome/free-solid-svg-icons";
 
-export const ProjectsList = ({title, items=[], limit, suffle = false}) => {
+export const ProjectsList = ({title, showSearchBar, searchPlaceHolder, items=[], limit, suffle = false}) => {
   const suffledItems = suffle ? items.sort(() => Math.random() - 0.5) : items
+
+  const [searchItem, setSearchItem] = useState("");
+  const [searchResults, setSearchResults] = useState(suffledItems);
+
+  const handleSearch = async (searchItem) => {
+    const results = suffledItems.filter(item =>
+      item.title.toLowerCase().includes(searchItem)
+    );
+    setSearchItem(searchItem);
+    setSearchResults(results);
+  }
 
   return (
     <div className="project-list-component">
-      <Container>
-        {!title || (
+      {!title || (
+        <div className="header-component">
           <Row>
             <Col>
-              <h1 className="title">{title}</h1>
+              <h2 className="title"><FontAwesomeIcon className="icon" icon={faCode} /> {title}</h2>
             </Col>
           </Row>
-        )}
+        </div>        
+      )}
+      {showSearchBar ? 
+        <div className="projects-search">
+            {showSearchBar ? <SearchBar input={searchItem} handleSearch={handleSearch} placeHolder={searchPlaceHolder} />: null}
+        </div> 
+      : null}
+      <Container>      
         <Row>
-          {suffledItems.map((item, i) => !limit || i < limit ? (
+          {searchResults.map((item, i) => !limit || i < limit ? (
             <ProjectListItem
               key={i}
               {...item}
@@ -40,6 +59,8 @@ export const ProjectsList = ({title, items=[], limit, suffle = false}) => {
 
 ProjectsList.propTypes = {
   title: PropTypes.string,
+  showSearchBar: PropTypes.bool,
+  searchPlaceHolder: PropTypes.string,
   items: PropTypes.array,
   limit: PropTypes.number,
   suffle: PropTypes.bool,
