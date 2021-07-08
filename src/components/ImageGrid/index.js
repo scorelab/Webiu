633bb223b2b51b98a367bbbacef494b89f38059e
Yash Vardhan <yashvardhan513@gmail.com>
2021-07-08 12:43:29 +0530

    add the category section in image-grid

--- src/components/ImageGrid/index.js
+++ src/components/ImageGrid/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import PropTypes from "prop-types"
 import "./style.sass"
-import {Row, Col} from 'react-bootstrap'
-import {withPrefix} from 'gatsby'
+import {Container, Row, Col} from 'react-bootstrap'
+import {navigate, withPrefix} from 'gatsby'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faImage } from '@fortawesome/free-solid-svg-icons';
 
-export const ImageGrid = ({images, size}) => {
+export const ImageGrid = ({header, images, size, categories, categoryData}) => {
 
   const renderImages = () => {
     return (
@@ -21,12 +23,37 @@ export const ImageGrid = ({images, size}) => {
 
   return (
     <div className="image-grid-component">
-      {images && images.length > 0 ? renderImages() : <p>No screen shots found!</p>}
+      {header ? <div className="header-component">
+          <h2 className="title">
+            <FontAwesomeIcon className="icon" icon={faImage} /> {header}
+          </h2>
+      </div> : null}
+      {images && images.length > 0 ? renderImages() : !categories ?  
+                                   <p>No screen shots found!</p> : null}
+      <Container>
+        <Row>
+          {categories ?
+            categoryData.map((category, id) => (
+              <Col md={size} className="category-col" key={id}>
+                <div className="category-div" 
+                     style={{backgroundColor: category.color}}  
+                     onClick={() => navigate(category.route)}>
+                  <h3 className="category-text">{category.text}</h3>
+                </div>
+              </Col>
+            ))
+          : null}
+        </Row>  
+      </Container>  
     </div>
   )
 }
 
 
 ImageGrid.propTypes = {
-  images: PropTypes.array
+  images: PropTypes.array,
+  header: PropTypes.string,
+  size: PropTypes.number,
+  categories: PropTypes.bool,
+  categoryData: PropTypes.array
 }
