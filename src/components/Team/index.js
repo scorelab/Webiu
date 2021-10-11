832f3bcabf155b3393941fde39c1a73e23fb0ef3
SaurabhSonde <saurabhsonde111@gmail.com>
2021-10-11 10:25:07 +0530

    added search bar for other team members

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,66 +1,124 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
-import {Container, Row, Col, Modal } from 'react-bootstrap'
-import TeamListItem from './TeamListItem'
+import { Container, Row, Col, Modal } from "react-bootstrap"
+import TeamListItem from "./TeamListItem"
 import "./style.sass"
-import {SearchBar} from "../SearchBar"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUsers } from '@fortawesome/free-solid-svg-icons';
+import { SearchBar } from "../SearchBar"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faUsers } from "@fortawesome/free-solid-svg-icons"
 
-export const Team = ({ title, showSearchBar, heads, contributors, researchers, alumni }) => {
-  const [searchItem, setSearchItem] = useState("");
-  const [searchResults, setSearchResults] = useState(contributors);
-  const [name, setName] = useState(null);
-  const [position, setPosition] = useState(null);
-  const [show, setShow] = useState(false);
-  const [image, setImage] = useState(null);
+export const Team = ({
+  title,
+  showSearchBar,
+  heads,
+  contributors,
+  researchers,
+  alumni,
+}) => {
+  const [searchItem, setSearchItem] = useState("")
+
+  const [headsSearchItem, setHeadsSearchItem] = useState("")
+  const [headsSearchResults, setHeadsSearchResults] = useState(heads)
+  const [researchersSearchItem, setResearchersSearchItem] = useState("")
+  const [researchersSearchResults, setResearchersSearchResults] = useState(
+    researchers
+  )
+  const [alumniSearchItem, setAlumniSearchItem] = useState("")
+  const [alumniSearchResults, setAlumniSearchResults] = useState(alumni)
+  const [searchResults, setSearchResults] = useState(contributors)
+  const [name, setName] = useState(null)
+  const [position, setPosition] = useState(null)
+  const [show, setShow] = useState(false)
+  const [image, setImage] = useState(null)
 
   const handleClick = (name, position, image) => {
-    setName(name);
-    setPosition(position);
+    setName(name)
+    setPosition(position)
     setImage(image)
-    setShow(true);
+    setShow(true)
   }
 
-  const handleSearch = async (searchItem) => {
+  const handleHeadsSearch = async headsSearchItem => {
+    const results = heads.filter(head =>
+      head.name.toLowerCase().includes(headsSearchItem)
+    )
+    setHeadsSearchItem(headsSearchItem)
+    setHeadsSearchResults(results)
+  }
+
+  const handleResearcherSearch = async researchersSearchItem => {
+    const results = researchers.filter(researcher =>
+      researcher.name.toLowerCase().includes(researchersSearchItem)
+    )
+    setResearchersSearchItem(researchersSearchItem)
+    setResearchersSearchResults(results)
+  }
+
+  const handleAlumniSearch = async alumniSearchItem => {
+    const results = alumni.filter(aAlumni =>
+      aAlumni.name.toLowerCase().includes(alumniSearchItem)
+    )
+    setAlumniSearchItem(alumniSearchItem)
+    setAlumniSearchResults(results)
+  }
+
+  const handleSearch = async searchItem => {
     const results = contributors.filter(contributor =>
       contributor.name.toLowerCase().includes(searchItem)
-    );
-    setSearchItem(searchItem);
-    setSearchResults(results);
+    )
+    setSearchItem(searchItem)
+    setSearchResults(results)
   }
 
   return (
     <div className="team-component">
-
       <Modal show={show} onHide={() => setShow(false)}>
         <Modal.Header closeButton></Modal.Header>
         <Modal.Body>
-           <div style={{textAlign: "center"}}>
-             <img src={image} alt="profile" className="team-img" />
-             <div><h4 className="team-title">{name}</h4></div>
-             <div><p className="team-position">{position}</p></div>
-           </div>
+          <div style={{ textAlign: "center" }}>
+            <img src={image} alt="profile" className="team-img" />
+            <div>
+              <h4 className="team-title">{name}</h4>
+            </div>
+            <div>
+              <p className="team-position">{position}</p>
+            </div>
+          </div>
         </Modal.Body>
       </Modal>
 
-      {title ? 
+      {title ? (
         <div className="header-component">
-          <h2><FontAwesomeIcon icon={faUsers} className="icon-h2" /> {title}</h2>
-        </div> 
-      : null}
+          <h2>
+            <FontAwesomeIcon icon={faUsers} className="icon-h2" /> {title}
+          </h2>
+        </div>
+      ) : null}
       <Container>
         {!heads || (
           <div>
-            <h3>Heads</h3>
-            <Row>
-              {heads.map((item, i) => (
-                <Col md={6} key={i} className="team-column" 
-                     onClick={() => handleClick(item.name, item.title, item.image)}>
-                  <TeamListItem 
-                    name={item.name}
-                    title={item.title}
+            <div className="team-search-div">
+              <h3>Heads</h3>
+              <div className="contributors-search">
+                {showSearchBar ? (
+                  <SearchBar
+                    input={headsSearchItem}
+                    handleSearch={handleHeadsSearch}
+                    placeHolder="Search heads"
                   />
+                ) : null}
+              </div>
+            </div>
+
+            <Row>
+              {headsSearchResults.map((item, i) => (
+                <Col
+                  md={6}
+                  key={i}
+                  className="team-column"
+                  onClick={() => handleClick(item.name, item.title, item.image)}
+                >
+                  <TeamListItem name={item.name} title={item.title} />
                 </Col>
               ))}
             </Row>
@@ -68,59 +126,101 @@ export const Team = ({ title, showSearchBar, heads, contributors, researchers, a
         )}
         <br />
 
-        {!researchers || 
-          <div>
-            <h3>Researchers</h3>
-            <Row>
-              {researchers.map((item, i) => (
-                <Col md={4} sm={6} key={i} className="team-column" 
-                     onClick={() => handleClick(item.name, item.title, item.image)}>
-                  <TeamListItem 
-                    name={item.name}
-                    title={item.title}
-                    horizontal
-                  />
-                </Col>
-              ))}
-            </Row>
-            <br />
-          </div>
-        }
-
-        {!alumni || 
-          <div>
-            <h3>Alumni</h3>
-            <Row>
-              {alumni.map((item, i) => (
-                <Col md={4} sm={6} key={i} className="team-column" 
-                     onClick={() => handleClick(item.name, item.title, item.image)}>
-                  <TeamListItem 
-                    name={item.name}
-                    title={item.title}
-                    horizontal
-                  />
-                </Col>
-              ))}
-            </Row>
-            <br />
-          </div>
-        }
-
-        {!contributors ||
+        {!researchers || (
           <div>
             <div className="team-search-div">
-              <h3>Contributors</h3> 
+              <h3>Researchers</h3>
               <div className="contributors-search">
-                {showSearchBar ? 
-                  <SearchBar input={searchItem} handleSearch={handleSearch} placeHolder="Search Contributors" />
-                : null}
+                {showSearchBar ? (
+                  <SearchBar
+                    input={researchersSearchItem}
+                    handleSearch={handleResearcherSearch}
+                    placeHolder="Search Researchers"
+                  />
+                ) : null}
+              </div>
+            </div>
+            <Row>
+              {researchersSearchResults.map((item, i) => (
+                <Col
+                  md={4}
+                  sm={6}
+                  key={i}
+                  className="team-column"
+                  onClick={() => handleClick(item.name, item.title, item.image)}
+                >
+                  <TeamListItem
+                    name={item.name}
+                    title={item.title}
+                    horizontal
+                  />
+                </Col>
+              ))}
+            </Row>
+            <br />
+          </div>
+        )}
+
+        {!alumni || (
+          <div>
+            <div className="team-search-div">
+              <h3>Alumni</h3>
+              <div className="contributors-search">
+                {showSearchBar ? (
+                  <SearchBar
+                    input={alumniSearchItem}
+                    handleSearch={handleAlumniSearch}
+                    placeHolder="Search Alumni"
+                  />
+                ) : null}
+              </div>
+            </div>
+
+            <Row>
+              {alumniSearchResults.map((item, i) => (
+                <Col
+                  md={4}
+                  sm={6}
+                  key={i}
+                  className="team-column"
+                  onClick={() => handleClick(item.name, item.title, item.image)}
+                >
+                  <TeamListItem
+                    name={item.name}
+                    title={item.title}
+                    horizontal
+                  />
+                </Col>
+              ))}
+            </Row>
+            <br />
+          </div>
+        )}
+
+        {!contributors || (
+          <div>
+            <div className="team-search-div">
+              <h3>Contributors</h3>
+              <div className="contributors-search">
+                {showSearchBar ? (
+                  <SearchBar
+                    input={searchItem}
+                    handleSearch={handleSearch}
+                    placeHolder="Search Contributors"
+                  />
+                ) : null}
               </div>
             </div>
             <Row>
               {searchResults.map((item, i) => (
-                <Col md={3} sm={4} key={i} className="team-column" 
-                     onClick={() => handleClick(item.name, item.title, item.image)}>
-                  <TeamListItem 
+                <Col
+                  md={3}
+                  sm={4}
+                  key={i}
+                  className="team-column"
+                  onClick={() => handleClick(item.name, item.title, item.image)}
+                >
+                  <TeamListItem
                     name={item.name}
                     title={item.title}
                     horizontal
@@ -130,8 +230,7 @@ export const Team = ({ title, showSearchBar, heads, contributors, researchers, a
             </Row>
             <br />
           </div>
-        }
-
+        )}
       </Container>
     </div>
   )
@@ -143,5 +242,5 @@ Team.propTypes = {
   alumni: PropTypes.array,
   contributors: PropTypes.array,
   title: PropTypes.string,
-  showSearchBar: PropTypes.bool
+  showSearchBar: PropTypes.bool,
 }
