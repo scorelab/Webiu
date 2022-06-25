ff65151dbfc1f8fa1cc19bb3de33a6d43c7b0975
wishwashan <ravindu.18@cse.mrt.ac.lk>
2022-06-25 14:16:13 +0530

    implemet theme 1 navigation bar

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,16 @@
+import React from "react"
+import { addDecorator } from "@storybook/react"
+import { ThemeProvider } from "@material-ui/core/styles"
+import theme from "../src/styles/theme"
+
+addDecorator(story => <ThemeProvider theme={theme}>{story()}</ThemeProvider>)
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   options: {
     storySort: (a, b) =>
-      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+      a[1].kind === b[1].kind
+        ? 0
+        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
-} 
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,65 @@
+import { createTheme } from "@material-ui/core/styles"
+
+const verdigris = "#47b3a3"
+const lightText = "#ffffff"
+const darkText = "#000000"
+const lightVerdigris = "#5cc4b4"
+const greyText = "#9e9e9e"
+const cardOldLace = "#fbf3ea"
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: verdigris,
+      light: lightText,
+      dark: darkText,
+    },
+    secondary: {
+      main: lightVerdigris,
+    },
+    common: {
+      grey: greyText,
+      oldlace: cardOldLace,
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: 32,
+      fontFamily: "Inter",
+      fontStyle: "italic",
+      fontWeight: "bold",
+      color: darkText,
+    },
+    h2: {
+      fontSize: 24,
+      fontFamily: "Inter",
+      color: lightText,
+      fontWeight: "bold",
+    },
+    h3: {
+      fontSize: 24,
+      fontFamily: "Inter",
+      color: darkText,
+      fontWeight: "bold",
+    },
+    h4: {
+      fontSize: 20,
+      fontFamily: "Inter",
+      color: lightText,
+    },
+    h5: {
+      fontSize: 15,
+      fontFamily: "Inter",
+      color: darkText,
+      fontWeight: "bold",
+    },
+    body1: {
+      fontSize: 20,
+      fontFamily: "Inter",
+      color: darkText,
+    },
+  },
+  overrides: {},
+})
+
+export default theme
