b54c44ae49566bd23f89b3883a248369fb0e8f31
wishwashan <ravindu.18@cse.mrt.ac.lk>
2022-08-06 12:46:54 +0530

    implement search bar component

---
+++ themes/learning-management-system/learning-management-theme/src/pages/groups.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../ui/layout"
+import SearchBar from "../component/group/searchBar"
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <SearchBar />
+    </Layout>
+  )
+}
+
+export default HomePage
