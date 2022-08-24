b6df5779616eef371ae0a610bead0367ce378970
wishwashan <ravindu.18@cse.mrt.ac.lk>
2022-08-25 00:41:24 +0530

    implement rooms page

---
+++ themes/hotel-management-system/hotel-management-theme/src/components/booking/bookingCard/index.js
@@ -0,0 +1,98 @@
+import React from "react"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import Divider from "@material-ui/core/Divider"
+import Button from "@material-ui/core/Button"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons"
+import Styles from "./styles"
+
+const BookingCard = ({
+  imgUrl,
+  title,
+  description,
+  roomId,
+  moreInfoList,
+  availability,
+}) => {
+  const classes = Styles({ availability })
+
+  const moreInfo = moreInfoList.map(ele => (
+    <Grid item key={ele.id} classes={{ root: classes.detailStyle }}>
+      <Button
+        startIcon={
+          <FontAwesomeIcon icon={ele.icon} className={classes.iconStyle} />
+        }
+        classes={{ root: classes.normalBtnStyle }}
+      >
+        {ele.key}: {ele.value}
+      </Button>
+    </Grid>
+  ))
+  return (
+    <Grid container direction="column" classes={{ root: classes.container }}>
+      <Grid item>
+        <img src={imgUrl} className={classes.imgStyle} />
+      </Grid>
+      <Grid container direction="column" classes={{ root: classes.wrapper }}>
+        <Grid item>
+          <Typography align="center">{title}</Typography>
+        </Grid>
+        <Grid item>
+          <Divider classes={{ root: classes.divideStyle }} />
+        </Grid>
+        <Grid item classes={{ root: classes.space }}>
+          <Typography variant="h6">{description}</Typography>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Grid
+          container
+          direction="column"
+          classes={{ root: classes.fullWidthContainer }}
+        >
+          <Grid item container justifyContent="center">
+            <Typography variant="h4">Room Number: {roomId}</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        container
+        direction="column"
+        classes={{ root: classes.wrapper }}
+      >
+        <Grid item container direction="row">
+          {moreInfo}
+        </Grid>
+        <Grid
+          item
+          container
+          direction="row"
+          classes={{ root: classes.space }}
+          justifyContent="space-around"
+        >
+          <Grid item>
+            <Button
+              startIcon={
+                availability ? (
+                  <FontAwesomeIcon icon={faCheck} />
+                ) : (
+                  <FontAwesomeIcon icon={faXmark} />
+                )
+              }
+              classes={{ root: classes.availableBtnStyle }}
+            >
+              {availability ? "Available" : "Unavailable"}
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button classes={{ root: classes.bookBtnStyle }}>Book Now</Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default BookingCard
--- themes/hotel-management-system/hotel-management-theme/src/components/booking/styles.js
+++ themes/hotel-management-system/hotel-management-theme/src/components/booking/styles.js
@@ -2,58 +2,27 @@ import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
 
 const Styles = makeStyles(theme => ({
-  container: {
-    border: `2px solid ${theme.palette.common.grey}`,
-    borderRadius: 10,
-    width: "250px",
-  },
-  imgStyle: {
-    height: "150px",
-    width: "246px",
-    borderTopLeftRadius: 8,
-    borderTopRightRadius: 8,
-  },
-  divideStyle: {
-    background: theme.palette.primary.dark,
-  },
-  priceStyle: {
-    ...theme.typography.h6,
-    color: theme.palette.common.tomato,
-  },
-  normalBtnStyle: {
-    border: `2px solid ${theme.palette.primary.main}`,
-    borderRadius: 20,
-    textTransform: "none",
+  decoratedHeader: {
+    ...theme.typography.h3,
     color: theme.palette.primary.main,
-    minWidth: "100px",
   },
-  wrapper: {
-    padding: "10px",
+  itemContainer: {
+    width: "90%",
+    "& > *": {
+      marginRight: "calc((100% - (250px * 4))/3)",
+      marginBottom: "40px",
+    },
+    "& > :nth-child(4n)": {
+      marginRight: 0,
+    },
   },
-  fullWidthContainer: {
-    backgroundColor: theme.palette.primary.main,
+  headerStyle: {
+    marginTop: "20px",
+    marginBottom: "20px",
   },
-  space: {
-    marginTop: "10px",
-  },
-  iconStyle: { color: theme.palette.primary.main },
-  detailStyle: { margin: "5px" },
-  avaiableBtnStyle: {
-    borderRadius: 20,
-    backgroundColor: theme.palette.primary.main,
-    color: props =>
-      props.availability
-        ? theme.palette.primary.light
-        : theme.palette.common.tomato,
-    "&:hover": { backgroundColor: theme.palette.primary.main },
-    textTransform: "none",
-  },
-  bookBtnStyle: {
-    backgroundColor: theme.palette.common.tomato,
-    color: theme.palette.primary.light,
-    borderRadius: 20,
-    "&:hover": { backgroundColor: theme.palette.common.tomato },
-    textTransform: "none",
+  selectStyle: {
+    marginLeft: "-80%",
+    marginBottom: "20px",
   },
 }))
 
