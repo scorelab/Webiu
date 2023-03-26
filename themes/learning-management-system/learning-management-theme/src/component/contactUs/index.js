1ff60f533e7dad699baffaf55a3e5f0db7fcf0d2
dhruv <dhruv18ranjan@gmail.com>
2023-03-26 16:08:54 +0530

    ui fixed

--- themes/learning-management-system/learning-management-theme/src/component/contactUs/index.js
+++ themes/learning-management-system/learning-management-theme/src/component/contactUs/index.js
@@ -23,66 +23,78 @@ const ContactUs = () => {
         classes={{ root: classes.wrapper }}
       >
         <Grid item>
-          <FontAwesomeIcon icon={iconName} size="2x" />
+          <FontAwesomeIcon icon={iconName} size="4x" style={{marginBottom:"20px"}} />
         </Grid>
-        <Grid item> {children}</Grid>
+        <Grid > {children}</Grid>
       </Grid>
     )
   }
   return (
     <Grid container direction="column" classes={{ root: classes.container }}>
       <Grid item>
-        <Typography variant="h4">Feel free to Contact Us!</Typography>
+        <Typography variant="h4"  classes={{root: classes.heading}} >Feel free to Contact Us!</Typography>
       </Grid>
       <Grid item classes={{ root: classes.space }}>
-        <Typography>
+        <Typography  classes={{root: classes.head}}>
           Please contact us using the information below. For additional
           informations on UOM Higher Education Institute
         </Typography>
       </Grid>
-      <Grid item container direction="column" classes={{ root: classes.space }}>
-        <Grid item>
-          <DetailCard iconName={faAddressCard}>
+      
+      <Grid item container direction="row" classes={{ root: classes.space }}>
+
+        <Grid item  classes={{root: classes.pad }}>
+          <DetailCard iconName={faAddressCard}   >
             <Grid container direction="column">
-              <Grid item>
-                <Typography>No: 07/A,</Typography>
+              <Grid item  classes={{root: classes.textGrid}}>
+                <Typography>Visit us</Typography>
               </Grid>
-              <Grid item>
-                <Typography> Galle Rd</Typography>
-              </Grid>
-              <Grid item>
-                <Typography>Galle</Typography>
+              <Grid item classes={{root: classes.textGrid}}>
+                <Typography>No: 07/A, <br></br> Galle Rd , Galle</Typography>
               </Grid>
             </Grid>
           </DetailCard>
         </Grid>
-        <Grid item classes={{ root: classes.spaceWrapper }}>
-          <DetailCard iconName={faBlenderPhone}>
+
+        <Grid item classes={{root: classes.pad }}>
+          <DetailCard iconName={faBlenderPhone} >
             <Grid container direction="column">
-              <Grid item>
+            <Grid item  classes={{root: classes.textGrid}}>
+                <Typography>Text us</Typography>
+              </Grid>
+              <Grid item classes={{root: classes.textGrid}}>
                 <Typography>091-5623123</Typography>
               </Grid>
             </Grid>
           </DetailCard>
         </Grid>
-        <Grid item>
-          <DetailCard iconName={faPhone}>
+
+        <Grid item classes={{root: classes.pad }}>
+          <DetailCard iconName={faPhone} size="6x" >
             <Grid container direction="column">
-              <Grid item>
+            <Grid item  classes={{root: classes.textGrid}}>
+                <Typography>Call us</Typography>
+              </Grid>
+              <Grid item classes={{root: classes.textGrid}}>
                 <Typography>076-3459011</Typography>
               </Grid>
             </Grid>
           </DetailCard>
         </Grid>
-        <Grid item>
-          <DetailCard iconName={faEnvelope}>
+
+        <Grid item classes={{root: classes.pad }}>
+          <DetailCard iconName={faEnvelope} >
             <Grid container direction="column">
-              <Grid item>
+            <Grid item  classes={{root: classes.textGrid}}>
+                <Typography>Mail us</Typography>
+              </Grid>
+              <Grid item classes={{root: classes.textGrid}}>
                 <Typography>info@uom.io</Typography>
               </Grid>
             </Grid>
           </DetailCard>
         </Grid>
+
       </Grid>
     </Grid>
   )
